2fe0633e289697909d73d5f8348f6aaa174134b8
Emily Ly <em.a.ly63@gmail.com>
2021-06-28 22:48:07 -0400

    added first few js

---
+++ portfolio.js
@@ -0,0 +1,37 @@
+var lyrics1 = document.querySelector('#lyrics1')
+function pinkLyrics() {
+    lyrics1.style.color = 'hotpink';
+};
+function turquoiseLyrics() {
+    lyrics1.style.color = 'turquoise';
+};
+
+var sendingg = document.querySelector('#message_send');
+function sending() {
+    sendingg.value = "Message Sent";
+};
+
+var lyrics2 = document.querySelector('#lyrics2');
+function showLyrics() {
+    lyrics2.style.display = 'block';
+};
+function hideLyrics() {
+    lyrics2.style.display = 'none';
+};
+
+function great(element) {
+    element.style.backgroundColor = 'palegreen';    
+};
+
+function good(element) {
+    element.style.backgroundColor = 'palegoldenrod';    
+};
+
+function okay(element) {
+    element.style.backgroundColor = 'palevioletred';    
+};
+    
+function out(element) {
+    element.style.backgroundColor = "lavender";   
+};
+
